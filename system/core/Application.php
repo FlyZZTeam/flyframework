6f8243759017074d6e8d4279a6b5ebcad02084fd
flyzz <zz@flyzz.net>
2015-02-25 11:29:41 +0800

    Add file header
    Add file header

--- system/core/Application.php
+++ system/core/Application.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * @link http://www.flyframework.com/
+ * @copyright Copyright &copy; FlyZZ Team
+ * @license http://www.flyframework.com/license.html
+ * @author zz <zz@flyzz.net>
+ */
 
 abstract class Application extends Module
 {
